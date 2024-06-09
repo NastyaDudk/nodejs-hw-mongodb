e0d4a1d2e160dc4840dfbebaa3010cec3410de97
Anastasiia <144330798+NastyaDudk@users.noreply.github.com>
2024-06-10 00:45:13 +0400

    ok

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,11 +1,21 @@
 import { ContactsCollection } from '../db/models/contact.js';
 
 export const getAllContacts = async () => {
-  const contacts = await ContactsCollection.find();
-  return contacts;
+  try {
+    const contacts = await ContactsCollection.find();
+    return contacts;
+  } catch (error) {
+    console.error('Error while fetching all contacts:', error);
+    return [];
+  }
 };
 
 export const getContactById = async (id) => {
-  const contact = await ContactsCollection.findById(id);
-  return contact;
+  try {
+    const contact = await ContactsCollection.findOne({ _id: id });
+    return contact;
+  } catch (error) {
+    console.error('Error while fetching contact by id:', error);
+    return null;
+  }
 };
